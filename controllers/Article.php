520701346b3e658ef1950c8e1422cd07aac1d4bf
Marie CHARLES <marie.charles@hetic.net>
2017-05-16 17:32:02 +0200

    toutes les pages du front-office ok

--- controllers/Article.php
+++ controllers/Article.php
@@ -6,7 +6,7 @@ use models\ArticleModel;
 
 class Article extends Controller
 {
-    protected function Articles() {
+    protected function Index() {
         $viewmodel = new ArticleModel();
         $this->render('article-detail.php', $viewmodel->pageArticle());
     }
