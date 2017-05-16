0d80b298bc167b87620506ae2f08738ee65434c3
Marie CHARLES <marie.charles@hetic.net>
2017-05-17 00:09:30 +0200

    avancement + code de Léa

--- views/Admin/index.php
+++ views/Admin/index.php
@@ -5,49 +5,83 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/css/screen.css">
+    <title>larchitecture.com - Interface Admin</title>
 </head>
 <body>
-<h1>Panel admin</h1>
+<h1>Bienvenue Administrateur</h1>
 
-<h2>Revues</h2>
+<div class="wrapper-list">
 
-<?php
+    <h2>Gérer les revues</h2>
 
-foreach ($viewmodel['revues'] as $revue): ?>
+    <table>
+        <tr>
+            <th>N°</th>
+            <th>Région</th>
+            <th>Date</th>
+        </tr>
 
-    <div>
-        <a href="<?='revues/getPageMagazine/' . $revue['id']?>">
-            <div><?= $revue['num'] ?></div>
-            <div><?= $revue['zone'] ?></div>
-            <div><?= $revue['date'] ?></div>
-            <img src="/architecture.com/assets/img-content/<?= $revue['img'] ?>" alt="">
-        </a>
-    </div>
+        <?php
 
-<?php endforeach; ?>
+        foreach ($viewmodel['revues'] as $revue): ?>
+
+        <tr>
+            <td><?= $revue['num'] ?></td>
+            <td><?= $revue['zone'] ?></td>
+            <td><?= $revue['date'] ?></td>
+            <td>
+                <a href="resume.php?id=<?=$row['id']?>">Modifier</a>
+            </td>
+            <td>
+                <form method="post" action="delete.php">
+                    <input type="hidden" value="<?= $row['id']?>" name ="id">
+                    <input type="submit" value="Supprimer">
+                </form>
+            </td>
+        </tr>
+
+        <?php endforeach; ?>
+
+    </table>
 
 
-<h2>Actu</h2>
+    <h2>Gérer les actualités</h2>
 
-<?php
+    <table>
+        <tr>
+            <th>Architecte</th>
+            <th>Réalisation</th>
+            <th>Date</th>
+        </tr>
 
-foreach ($viewmodel['actu'] as $actu):
+        <?php
 
-    ?>
+        foreach ($viewmodel['actu'] as $actu):
 
-    <div><a href="<?='getPageActu/' . $actu['id']?>">
-            <div><?= $actu['date'] ?></div>
-            <div><?= $actu['architecte'] ?></div>
-            <div><?= $actu['realisations'] ?></div>
-            <div><?= $actu['rubrique'] ?></div>
-            <div><?= $actu['lieu'] ?></div>
-            <div><?= $actu['departement'] ?></div>
-            <div><?= $actu['contenu'] ?></div>
-            <img src="assets/img-content/<?= $actu['img'] ?>" alt="">
-    </div></a>
+        ?>
 
-<?php endforeach; ?>
+            <tr>
+                <td><?= $actu['architecte'] ?></td>
+                <td><?= $actu['réalisations'] ?></td>
+                <td><?= $actu['date'] ?></td>
+                <td>
+                    <a href="resume.php?id=<?=$row['id']?>">Modifier</a>
+                </td>
+                <td>
+                    <form method="post" action="delete.php">
+                        <input type="hidden" value="<?= $row['id']?>" name ="id">
+                        <input type="submit" value="Supprimer">
+                    </form>
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+
+    </table>
+
+</div>
 
 </body>
 </html>
